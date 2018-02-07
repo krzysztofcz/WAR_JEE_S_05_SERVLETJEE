8e177a3a8dcb657ec5a641775ced18cbfde35ad0
krzysztofcz <34889794+krzysztofcz@users.noreply.github.com>
2018-02-07 12:35:02 +0100

    cookie ex #3

--- src/pl/coderslab/Cookie6.java
+++ src/pl/coderslab/Cookie6.java
@@ -29,23 +29,44 @@ public class Cookie6 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		Writer wr =response.getWriter();
+		Cookie[] ciastka = request.getCookies();
+		String remTextColor = null;
+		String remBackGroundColor = null;
+		for ( Cookie ciastko : ciastka) {
+			if( ciastko.getName().equals("textColor")) {
+				remTextColor = ciastko.getValue() ;
+			} else if (ciastko.getName().equals("backgroundColor")) {
+				remBackGroundColor = ciastko.getValue() ;
+			}
+		}
+		if(remTextColor!=null && remBackGroundColor!=null) {
+			wr.append("Zapamiętany w ciasteczku kolor tekstu to: "+remTextColor+", oraz tła jako: "+remBackGroundColor+" .");
+		}
+		if(remTextColor==null && remBackGroundColor==null) {
+			response.sendRedirect("index_6.html");
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Writer writer=response.getWriter();
+		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
+		Writer writer=response.getWriter();
 		String textColor=request.getParameter("textColor");
 		String backgroundColor=request.getParameter("backgroundColor");
 		Cookie ciastkoTC = new Cookie("textColor", textColor);
 		Cookie ciastkoBGC = new Cookie("backgroundColor", backgroundColor);
+		ciastkoTC.setMaxAge(3600*2);
+		ciastkoBGC.setMaxAge(3600*2);
 		response.addCookie(ciastkoTC);
 		response.addCookie(ciastkoBGC);
 		writer.append("Wybrałeś kolor tekstu jako: "+textColor+", oraz tła jako: "+backgroundColor+". Twój wybór został zapisane w ciasteczku.");
+		
 		
 	}
 
