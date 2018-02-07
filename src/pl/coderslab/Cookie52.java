a6665600934d18bb44e60724be1231ae1ae2259f
krzysztofcz <34889794+krzysztofcz@users.noreply.github.com>
2018-02-07 09:58:29 +0100

    cokie52

---
+++ src/pl/coderslab/Cookie52.java
@@ -0,0 +1,59 @@
+package pl.coderslab;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Cookie52
+ */
+@WebServlet("/Cookie52")
+public class Cookie52 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Cookie52() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		Writer writer = response.getWriter();
+		
+		try {
+			Cookie[] ciasteczka= request.getCookies();
+			for (Cookie ciastko:ciasteczka) {
+				if( ciastko.getName().equals("visited")&& ciastko.getValue().equals("Cookie51") ) {
+					writer.append("<p>Witamy na stronie Cookie52</p>");
+					ciastko.setMaxAge(0);
+					response.addCookie(ciastko);
+				} 
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			writer.append("Nie odwiedziłeś jeszcze tej strony<br>You must visit this site <a href=\"Cookie51\" >Cookie51</a> first to come here.");
+		} 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
